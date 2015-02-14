d38d8236a57f41fb697045778197f1013bed45ae
Marcie Van Auken <marcievanauken@gmail.com>
2015-02-14 15:34:44 -0500

    worked on the contact page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	$(".formButton").on("click", function() {
+			console.log("there was a click");
+		});
+
+		function sayHi() {
+			alert("Your message has been sent");
+		}
+
+	$(".formButton").on("click", sayHi);
+
+});
